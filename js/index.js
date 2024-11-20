87bf744dfacd245a8a44fb2f9fc5bea37bf7a138
Abdo15P <abdelrahman.aly.m@gmail.com>
2024-11-20 13:16:21 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -1,27 +1,119 @@
+var siteNameInput = document.getElementById("siteName"); 
+var siteURLInput = document.getElementById("siteURL"); 
 
-var quotes=[
-  {quote:'“Smart people learn from everything and everyone, average people from their experiences, stupid people already have all the answers.”'  ,  by:"― Socrates"},
-  {quote:'“The wheel of the world swings through the same phases again and again.”'  ,  by:"― Rudyard Kipling, The Man Who Would Be King"},
-  {quote:'“Insanity is doing the same thing, over and over again, but expecting different results.”'  ,  by:"― Narcotics Anonymous"},
-  {quote:'“Time you enjoy wasting is not wasted time.”'  ,  by:"― Marthe Troly-Curtin, Phrynette Married"},
-  {quote:'“Every night I dream a lot. Every day I live a little.”'  ,  by:"― Max Nowaz, Get Rich or Get Lucky"},
-  {quote:'“Live as if you were to die tomorrow. Learn as if you were to live forever.”'  ,  by:"― Mahatma Gandhi"},
-  {quote:'“Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.”'  ,  by:"― Martin Luther King Jr., A Testament of Hope: The Essential Writings and Speeches"},
-  {quote:'“Always forgive your enemies; nothing annoys them so much.”'  ,  by:"― Oscar Wilde"},
-  {quote:'“I have learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.”'  ,  by:"― Maya Angelou"},
-  {quote:'“If you want to know what a man is  like, take a good look at how he treats his inferiors, not his equals.”'  ,  by:"― J.K. Rowling, Harry Potter and the Goblet of Fire"},
-];
-var rand=0;
-var prevRand=0;
-function generateQuote(){
-  rand=Math.floor(Math.random() * quotes.length);
-  while(rand===prevRand){
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
 
-    rand=Math.floor(Math.random() * quotes.length);
+var bookmarkList = []; 
+
+if (localStorage.getItem("bookmarkContainer") !== null) {
+  bookmarkList = JSON.parse(localStorage.getItem("bookmarkContainer")); 
+  displayData(); 
+}
+
+
+
+function addBookmark() {
+  if (validateName() && validateURL()){
+ 
+    var bookmark = {
+      name: siteNameInput.value.trim(), 
+      url: siteURLInput.value.trim(), 
+    }
+
+    bookmarkList.push(bookmark);
+    
+    localStorage.setItem("bookmarkContainer", JSON.stringify(bookmarkList));
+
+    displayData(); 
+    siteNameInput.classList.remove("is-valid");
+    siteURLInput.classList.remove("is-valid");
+    clearForm(); 
   }
-  document.getElementById("quote").innerHTML=`
-  <p>"${quotes[rand].quote}</p>
-            <span>${quotes[rand].by}</span>
-            </div>`;
-  prevRand=rand;
-          }
+  else{
+      showMsg();
+    
+  }
+  }
+
+
+function displayData() {
+  var bookmarkTable = "";
+  for (var i = 0; i < bookmarkList.length; i++) {
+    bookmarkTable += htmlContent(i); 
+  }
+  
+  document.getElementById("content").innerHTML=bookmarkTable;
+}
+
+  function clearForm() {
+    siteNameInput.value=null;
+    siteURLInput.value=null;
+  }
+
+  function deleteBookmark(index) {
+    bookmarkList.splice(index, 1); 
+    localStorage.setItem("bookmarkContainer", JSON.stringify(bookmarkList));
+    displayData(); 
+  }
+function htmlContent(i){
+  return `<tr>
+                        <th class="pt-3" scope="row">${i+1}</th>
+                        <td class="pt-3" >${bookmarkList[i].name}</td>
+                        <td><a class="btn subBtn  px-3 text-white rounded-2" target="_blank" href="https://${bookmarkList[i].url}"
+                                role="button"><i class="fa-solid fa-eye"></i> Visit</a></td>
+                        <td><button onclick="deleteBookmark(${i})" type="submit" class="btn delBtn   text-white rounded-2 "><i
+                                    class="fa-solid fa-trash-can"></i> Delete</button></td>
+                    </tr>`
+}
+
+function validateName(){
+  var regex=/^(?:[a-zA-Z0-9]*[a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9]*)$/;
+  var text= siteNameInput.value;
+  var nameErr= document.getElementById("nameErr");
+
+  if(regex.test(text)){
+    siteNameInput.classList.add("is-valid");
+    siteNameInput.classList.remove("is-invalid");
+    nameErr.classList.add("d-none");
+    return true;
+  }
+  else{
+    siteNameInput.classList.add("is-invalid");
+    siteNameInput.classList.remove("is-valid");
+    nameErr.classList.remove("d-none");
+    return false;
+  }
+}
+
+function validateURL(){
+  var regex=/^(https?:\/\/)?([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})(\/[^\s]*)?$/;
+  var text= siteURLInput.value;
+  var urlErr= document.getElementById("urlErr");
+
+  if(regex.test(text)){
+    siteURLInput.classList.add("is-valid");
+    siteURLInput.classList.remove("is-invalid");
+    urlErr.classList.add("d-none");
+    return true;
+  }
+  else{
+    siteURLInput.classList.add("is-invalid");
+    siteURLInput.classList.remove("is-valid");
+    urlErr.classList.remove("d-none");
+    return false;
+  }
+}
+
+function showMsg() {
+  modal.style.display = "block";
+}
+function closeMsg() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+      modal.style.display = "none";
+  }
+}
