92baf6c97314d76776c622dc3c3fbd4c4ba93646
Abdo15P <abdelrahman.aly.m@gmail.com>
2024-12-01 15:23:05 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var siteNameInput = document.getElementById("siteName"); 
+/* var siteNameInput = document.getElementById("siteName"); 
 var siteURLInput = document.getElementById("siteURL"); 
 
 var modal = document.getElementById("myModal");
@@ -115,5 +115,69 @@ function closeMsg() {
 window.onclick = function(event) {
   if (event.target == modal) {
       modal.style.display = "none";
+  }
+}
+ */
+let usersList=[];
+var emailInput = document.getElementById("email");
+var passwordInput = document.getElementById("password"); 
+if (localStorage.getItem("usersContainer") !== null) {
+  usersList = JSON.parse(localStorage.getItem("usersContainer")); 
+}
+
+function validateUser(){
+  
+  var userErr1= document.getElementById("userErr1");
+  var userErr2= document.getElementById("userErr2");
+  
+      let flag=false;
+      if(validateEmail() ){
+        
+        for(let i=0;i<usersList.length;i++){
+          console.log("here")
+          if(emailInput.value === usersList[i].email && passwordInput.value === usersList[i].password){
+              console.log("enter");
+              localStorage.setItem("currentUser", JSON.stringify(usersList[i].name));
+              window.location.href= './home.html';
+              userErr1.classList.add("d-none");
+              userErr2.classList.add("d-none");
+              flag=true;
+          }
+          
+      }
+      if(flag === false){
+        userErr1.classList.remove("d-none");
+        userErr2.classList.add("d-none");
+      }
+      
+      }
+      else{
+        userErr1.classList.add("d-none");
+        userErr2.classList.remove("d-none");
+
+      }
+      
+      
+}
+
+
+function validateEmail(){
+  var regex=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  var text= emailInput.value;
+  var emailErr= document.getElementById("emailErr");
+ 
+  if(regex.test(text) ){
+      
+      emailInput.classList.add("is-valid");
+      emailInput.classList.remove("is-invalid");
+      emailErr.classList.add("d-none");
+      
+      return true;
+  }
+  else{
+    emailInput.classList.add("is-invalid");
+      emailInput.classList.remove("is-valid");
+      emailErr.classList.remove("d-none");
+      return false;
   }
 }
